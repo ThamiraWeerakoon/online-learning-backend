3473e01a0325c7fe72f4ecb78f08eba62e7566fd
ThamiraWeerakoon <thamira.tr@gmail.com>
2025-07-11 19:24:50 +0530

    Updated frontend origin.

--- server.js
+++ server.js
@@ -22,12 +22,11 @@ const app = express();
 //}));
 
 app.use(cors({
-    origin: 'online-learning-frontend-cnilgbyb3-thamira-weerakoons-projects.vercel.app',
+    origin: 'https://online-learning-frontend-cnilgbyb3-thamira-weerakoons-projects.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
 
 // Parse incoming JSON
 app.use(express.json());
